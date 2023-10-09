887673d2acdbb120d5d5b06a45710771f12af3f5
reik de vries <37676@ma-web.nl>
2023-10-09 12:21:46 +0200

    test

--- haiku.js
+++ haiku.js
@@ -6,11 +6,13 @@ function Haiku1(){
     console.log("How much more of it remains?");
     console.log("The night is brief.");
 }
-function HaikuX(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+function Haiku2(){
+        console.log("haiku by:","Reik de Vries");{
+        console.log("haiku by:","Docent");
+        console.log("Panda in the mist");
+        console.log("Gently chewing on bamboo");
+        console.log("Perfect breakfast time");
+    
 }
 function HaikuX(){
     console.log("haiku by:","AUTHOR");
