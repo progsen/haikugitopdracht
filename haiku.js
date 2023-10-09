8fd514ea6c8762944c84a782814d3046cd1be94e
Felix Huiskamp <37748@ma-web.nl>
2023-10-09 11:58:43 +0200

    test

--- haiku.js
+++ haiku.js
@@ -1,11 +1,26 @@
 
 
-//plak hier de haiku functions onder elkaar
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","AUTHOR");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
 
 
 
-
-//laat de code hieronder met rust
 Haiku1()
 Haiku2()
 Haiku3()
