e1b26dd4713f080b7c26084ca83392280fd4f3d2
Felix Huiskamp <37748@ma-web.nl>
2023-10-09 12:18:01 +0200

    edit

--- haiku.js
+++ haiku.js
@@ -1,10 +1,10 @@
 
 
-function HaikuX(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+function Haiku1(){
+    console.log("haiku by:","Felix Huiskamp");
+    console.log("My life");
+    console.log("How much more of it remains?");
+    console.log("The night is brief.");
 }
 function HaikuX(){
     console.log("haiku by:","AUTHOR");
