f052a7f00a250279d0b54fb5928333810e5a9bd8
isaiahajilo <28072@ma-web.nl>
2023-10-09 12:46:16 +0200

    'edit'

--- haiku.js
+++ haiku.js
@@ -14,15 +14,21 @@ function Haiku2(){
         console.log("Perfect breakfast time");
     
 }
-function HaikuX(){
+function Haiku3(){
     console.log("haiku by:","Amir");
     console.log("The Taste");
     console.log("of rain");
     console.log("Why kneel");
 }
-
+function Haiku4(){
+    console.log("haiku by:","isaiah");
+    console.log("i love banaan");
+    console.log("monkey lover");
+    console.log("never have i ever");
+}
 
 
 Haiku1()
 Haiku2()
 Haiku3()
+Haiku4()
