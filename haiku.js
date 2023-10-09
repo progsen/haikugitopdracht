335a507f2f1fb01e9f2ff9d81b08726bb3f06fcf
Amir <34164@ma-web.nl>
2023-10-09 12:38:55 +0200

    edit

--- haiku.js
+++ haiku.js
@@ -15,10 +15,10 @@ function Haiku2(){
     
 }
 function HaikuX(){
-    console.log("haiku by:","AUTHOR");
-    console.log("REGEL1");
-    console.log("REGEL2");
-    console.log("REGEL3");
+    console.log("haiku by:","Amir");
+    console.log("The Taste");
+    console.log("of rain");
+    console.log("Why kneel");
 }
 
 
